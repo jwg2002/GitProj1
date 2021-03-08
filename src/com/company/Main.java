6821d3e92f1efdb7d15af0e4a0bbed6cf6a9a8d9
jwg2002 <jwg_2002@163.com>
2021-03-08 19:13:23 +0800

    git proj1

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.sound.midi.Soundbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello");
+
+
+
+
+    }
+}
